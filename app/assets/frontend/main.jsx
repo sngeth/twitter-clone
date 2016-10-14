b8438ac39f1b214aa69d69a1ab35db96962b894c
Sid Ngeth <Sid.Ngeth@ascentis.com>
2016-10-14 12:56:31 -0400

    Added babel and webpack

---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,16 @@
+import Greet from './greet';
+
+class Main extends React.Component {
+  render() {
+    return(<Greet />);
+  }
+}
+
+let documentReady = () => {
+  ReactDOM.render(
+    <Main />,
+    document.getElementById('react')
+  );
+};
+
+$(documentReady);
