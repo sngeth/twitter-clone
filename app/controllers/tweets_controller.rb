32164c2a76e13da3cb2321ee41c32a4005d8dc3b
Sid Ngeth <sngeth@gmail.com>
2016-10-25 14:45:18 -0400

    Added moment.js and added timestamp to tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   def index
-    render json: Tweet.includes(:user).all
+    render json: Tweet.includes(:user).order(created_at: :desc).all
   end
 
   def create
